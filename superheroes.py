44730e4b7e2dc5918815b0296964fa175a4ea3e8
Ariane Evans <ariane.evans@student.makeschool.com>
2019-09-22 18:44:31 -0700

    [finished] Abiloty class

--- superheroes.py
+++ superheroes.py
@@ -1,7 +1,16 @@
-import random
+from random import randint
 
 class Ability:
     def __init__ (self, name, attack_strength):
         self.name = name
-        attack_strength = 10
+        self.max_damage = attack_strength
         pass
+    
+    def attack (self):
+        return randint(1, self.max_damage)
+
+
+if __name__ == "__main__":
+    ability = Ability ("Debugging Ability", 20)
+    print (ability.name)
+    print (ability.attack())
