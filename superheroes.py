7c1223e99bbfbe941b70e08f140373ed08996575
Ariane Evans <ariane.evans@student.makeschool.com>
2019-09-22 18:50:51 -0700

    [add] Armor class

--- superheroes.py
+++ superheroes.py
@@ -9,8 +9,16 @@ class Ability:
     def attack (self):
         return randint(1, self.max_damage)
 
+class Armor:
+    def __init__ (self, name, max_block):
+        self.name = name
+        self.max_block = max_block
+
+    def block (self):
+        return randint(1, self.max_block)
 
 if __name__ == "__main__":
     ability = Ability ("Debugging Ability", 20)
     print (ability.name)
     print (ability.attack())
+
